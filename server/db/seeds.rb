e285fd5ba53c31690a155722577c0e64e4e11f57
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-28 04:46:21 +0000

    change datatype 'wikipedia_node.word' from string to text

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,41 +6,52 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 elems_edge = {}
 
 elems_all = []
 records = []
 
-File.foreach("tmp/links_scraped.tsv") do |line|
+File.open("tmp/links_scraped_test.tsv").read.split(/\n/).each_with_index do |line, index|
   elems = line.chomp.split(/\t/).uniq
   elems_edge[elems[0]] = elems[1..-1]
-  if elems.size > 1
+  unless elems.empty?
     elems_all += elems
-    elems_all.uniq!
   end
 end
 
-elems_all.each do |word|
-  records <<  WikipediaNode.new({"word": word})
+elems_all.uniq!
+puts "finish loading tsv"
+
+elems_all.each_with_index do |word, index|
+  records.push(WikipediaNode.new({"word": word}))
 end
+
+puts "finish creating WikipediaNode instances"
 
 WikipediaNode.import records
 
+puts "finish importint records into WikipediaNode"
+
 wiki_node_dict = {}
 
-WikipediaNode.all.each do |record|
+WikipediaNode.all.each_with_index do |record, index|
   wiki_node_dict[record.word] = record.id
 end
 
+puts "finish getting edges"
+
 records = []
 
-elems_edge.each do |from, to|
+elems_edge.each_with_index do |(from, to), index|
   if from && to
     to.map{|to_word| wiki_node_dict[to_word]}.each do |to_id|
-      records << WikipediaEdge.new({"from_id": wiki_node_dict[from], "to_id": to_id})
+      records.push(WikipediaEdge.new({"from_id": wiki_node_dict[from], "to_id": to_id}))
     end
   end
 end
 
+puts "finish creating WikipediaEdge instances"
+
 WikipediaEdge.import records
+
+puts "finish importing records into WikipediaEdge"
