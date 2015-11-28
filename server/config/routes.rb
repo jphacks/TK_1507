e57ad58b99dc694d0088d9087bac5c35c0e91385
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-11-28 02:42:05 +0000

    MOD routes.rb(setting json response)

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get 'search', to: 'json_api_v1#search'
+  namespace :api, { format: 'json' } do
+      get 'search', to: 'json_api_v1#search'
+  end
+  
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
