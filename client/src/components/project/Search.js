08aec42b7fca48f571956e92450defa908319512
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-11-29 02:47:43 +0900

    ui fix

--- client/src/components/project/Search.js
+++ client/src/components/project/Search.js
@@ -9,13 +9,17 @@ var Search = React.createClass({
       <div className='container'>
         <h1>Search</h1>
         <hr />
+        <h2>紹介文</h2>
+        <p>hoge hoge huga hoge hoge huga hoge hoge huga hoge hoge huga hoge hoge huga</p>
 
         <div className='row'>
           <div className='container'>
             <div className='col-sm-8 col-sm-offset-2'>
-              <div className='card'>
-                <div className='card-block'>
-                  <SearchForm />
+              <div className='row'>
+                <div className='card'>
+                  <div className='card-block'>
+                    <SearchForm />
+                  </div>
                 </div>
               </div>
             </div>
