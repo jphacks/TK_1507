d8ca2892859a888aee79160e251aa0c1de7475d1
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-11-28 22:48:53 +0900

    update view(api connect)

---
+++ client/src/components/project/Result.js
@@ -0,0 +1,47 @@
+var React = require('react')
+
+var Result = React.createClass({
+  getInitialState: function() {
+    return {
+      result: []
+    };
+  },
+  componentWillMount: function() {
+    var self = this;
+    fetch('http://localhost:3000/api/v1/results/' + this.props.params.resultId, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(response) {
+      if (response !== null) {
+        console.log(response)
+        self.setState({result: response})
+      }
+    })
+  },
+  render() {
+    return (
+      <div className='container'>
+        <h1>Result</h1>
+        <hr />
+        <p>#{this.props.params.resultId}</p>
+
+        {
+          this.state.result.map(function(item, index) {
+            return (
+              <div className='col-xs-12 card' key={index}>
+                <p key={index}></p>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+})
+
+module.exports = Result
