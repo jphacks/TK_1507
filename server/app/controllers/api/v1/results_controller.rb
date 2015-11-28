350a0ce1edacc22efb39b8b4f7741ebc832601fa
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2015-11-29 03:12:38 +0900

    api mock

--- server/app/controllers/api/v1/results_controller.rb
+++ server/app/controllers/api/v1/results_controller.rb
@@ -1,35 +1,37 @@
-# -*- coding: undecided -*-
 class Api::V1::ResultsController < ApplicationController
   protect_from_forgery except: [:index, :create, :show]
 
   def index
     hs = Result.all
     render :json => hs
-  end  
-  
+  end
+
   def create
     param = searchParams
-    
-    ids = client.query("select id, word from wikipedia_nodes where word = '#{params[:from]}' or word = '#{params[:to]}'").to_a.map{|record| record['id']}
-    
+
+    # ids = client.query("select id, word from wikipedia_nodes where word = '#{params[:from]}' or word = '#{params[:to]}'").to_a.map{|record| record['id']}
+
     from_id = ids[0]
     to_id = ids[1]
-    
+
     chains = Array.new
     ########################
     # chains = awesomeMethod params[:from], params[:to]
     # => [@node, @node, @node]
-    # chains = [1, 2, 3, 4, 5]
+    chains = []
+    [1, 2, 3, 4, 5].each do |i|
+      chains.push WikipediaNode.create word: 'hoge'
+    end
     ########################
-    
-    chains.push(from_id)
-    chains.push(bfs(client, [], [], from_id, to_id))
-    chains.push(to_id)
+
+    # chains.push(from_id)
+    # chains.push(bfs(client, [], [], from_id, to_id))
+    # chains.push(to_id)
 
     result = Result.new
     if result.save
-      chains.each do |node_id|
-        result_item = ResultItem.new result_id: result.id, node_id: node_id
+      chains.each do |node|
+        result_item = ResultItem.new result_id: result.id, node_id: node.id
         result_item.save
       end
       render json: result
@@ -39,7 +41,8 @@ class Api::V1::ResultsController < ApplicationController
   end
 
   def show
-    ri = ResultItem.where(result_id: params['results_id'])
+    p params
+    ri = ResultItem.where(result_id: params['id'])
     render :json => ri
   end
 
@@ -47,15 +50,15 @@ class Api::V1::ResultsController < ApplicationController
   def searchParams
     params.require(:result).permit(:from, :to)
   end
-  
+
   private
   def bfs(client, open_list=[], check_list=[], from_id, to_id)
     records = client.query("select to_id from wikipedia_edges where from_id = '#{from_id}' limit 100").to_a.map{|record| record['to_id']}
     check_list.push from_id
     open_list.concat records
-    
+
     return true if records.include? to_id
-    
+
     records.each do |next_from_id|
       unless check_list.include? next_from_id
         bfs(client, open_list, check_list, next_from_id, to_id)
@@ -63,5 +66,5 @@ class Api::V1::ResultsController < ApplicationController
     end
     return 0
   end
-  
+
 end
