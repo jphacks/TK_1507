d6279fc9a4aea60af8d715bc67697057b0e7e1ae
himkt <himkt@klis.tsukuba.ac.jp>
2015-11-29 10:24:41 +0900

    Implement results/create

--- server/app/controllers/api/v1/results_controller.rb
+++ server/app/controllers/api/v1/results_controller.rb
@@ -8,33 +8,20 @@ class Api::V1::ResultsController < ApplicationController
 
   def create
     params.require(:result).permit(:from, :to)
+    
+    from = params["result"]["from"]
+    to = params["result"]["to"]
 
     from_id = WikipediaNode.find_by(word: from).id
     to_id = WikipediaNode.find_by(word: to).id
 
-    if path = bfs(client, open_list=[from_id], check_list=[], to_id, edges=[])
+    if path = bfs(open_list=[from_id], check_list=[], to_id, edges=[])
       chains = [to_id]
       while to_id
         to_id = path[to_id]
-        chains.push(to_id)
+        chains.push(to_id) if to_id
       end
     end
-
-    # ids = client.query("select id, word from wikipedia_nodes where word = '#{params[:from]}' or word = '#{params[:to]}'").to_a.map{|record| record['id']}
-
-    # from_id = ids[0]
-    # to_id = ids[1]
-
-    # chains = Array.new
-    ########################
-    # chains = awesomeMethod params[:from], params[:to]
-    # => [@node, @node, @node]
-    # chains = [1, 2, 3, 4, 5]
-    ########################
-
-    # chains.push(from_id)
-    # chains.push(bfs(client, [], [], from_id, to_id))
-    # chains.push(to_id)
 
     result = Result.new
     if result.save
@@ -55,15 +42,16 @@ class Api::V1::ResultsController < ApplicationController
   end
 
   private
-  def bfs(client, open_list, check_list=[], to_id, edges)
+  def bfs(open_list, check_list=[], to_id, edges)
 
     from_id = open_list.shift
     # puts "searching...#{from_id}"
 
-    records = WikipediaEdge.where(from_id: 300).map{|record| record.to_id}
+    records = WikipediaEdge.where(from_id: from_id)
+    .map{|record| record.to_id}
+    .select{|item| !item.nil?}
     # records = client.query("select to_id from wikipedia_edges where from_id = '#{from_id}'")
     # .to_a.map{|record| record['to_id']}
-    # .select{|item| !item.nil?}
 
     edges += records.map{|to_id| [from_id, to_id]}
 
@@ -75,7 +63,7 @@ class Api::V1::ResultsController < ApplicationController
     if records.include? to_id
       return edges.map{|edge| edge.reverse}.to_h
     else
-      return bfs(client, open_list, check_list, to_id, edges)
+      return bfs(open_list, check_list, to_id, edges)
     end
   end
 
